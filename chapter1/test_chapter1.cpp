d567f2f2af73db2b2bd40312fd56baba1cdb4fc3
Eric <eric.ganzert@gmail.com>
2021-03-28 10:40:21 -0700

    test out clang format

--- chapter1/test_chapter1.cpp
+++ chapter1/test_chapter1.cpp
@@ -73,5 +73,10 @@ TEST(CH1Test, basic)
         cout << "FAIL" << endl;
     }
 
+    // test out clang format
+    char varOne; char varTwo;
+    (void)varOne;
+    (void)varTwo;
+
     EXPECT_TRUE(allClear);
 }
