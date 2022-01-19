f1f7743365ecc55085078d147bb7f62f8199f7fc
Eric Ganzert <eganzert@teradici.com>
2022-01-19 09:58:56 -0800

    R-3.1 basic implementation of Scores::add and Scores::remove

--- chapter3/chapter3.cpp
+++ chapter3/chapter3.cpp
@@ -1,5 +1,7 @@
 #include "chapter3.h"
 
+#include <stdexcept>
+
 using namespace std;
 
 GameEntry::GameEntry(const string& n, int s)
@@ -23,4 +25,44 @@ Scores::Scores(int maxEnt)
 
 Scores::~Scores() {
     delete[] entries;
+}
+
+void Scores::add(const GameEntry& e)
+{
+    int newScore = e.getScore();
+    if (numEntries == maxEntries)
+    {
+        if (newScore <= entries[maxEntries-1].getScore())
+        {
+            return;
+        }
+    }
+    else {
+        numEntries++;
+    }
+
+    int i = numEntries - 2;
+    while (i >= 0 && entries[i].getScore() < newScore)
+    {
+        entries[i+1] = entries[i];
+        i--;
+    }
+    entries[i+1] = e;
+}
+
+GameEntry Scores::remove(int i)
+{
+    if (i < 0 || i >= numEntries)
+    {
+        throw runtime_error("Invalid index");
+    }
+
+    GameEntry e = entries[i];
+    for (int j = i+1; j < numEntries; j++)
+    {
+        entries[j-1] = entries[j];
+    }
+    
+    numEntries--;
+    return e;
 }
